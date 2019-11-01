b8625482c7145030f44a3f1ac27e3bc803ecb140
CreativePenguin <panda501wp@gmail.com>
2019-11-01 11:46:11 -0400

    Continue writing getrand()

--- Song_Node.c
+++ Song_Node.c
@@ -1,15 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 #include "Song_Node.h"
 
-void print_node(struct song_node * sn) {
+void print_node(struct song_node * sn) 
+{
 	if (sn != NULL) {
 		printf("%s: %s", sn->artist, sn->name);
 	}
 }
 
-void print_library(struct song_node * sn) {
+void print_library(struct song_node * sn) 
+{
 	while (sn != NULL) {
 		print_node(sn);
 		printf(" | ");
@@ -18,8 +21,9 @@ void print_library(struct song_node * sn) {
 	printf("\n");
 }
 
-struct song_node * insert_front(struct song_node * sn,\
-								char * artist, char * name) {
+struct song_node * insert_front(struct song_node * sn,
+								char * artist, char * name) 
+{
 	struct song_node * sn2 = malloc(sizeof(struct song_node));
 	strcpy(sn2->name, name);
 	strcpy(sn2->artist, artist);
@@ -41,12 +45,23 @@ int song_cmp(struct song_node * sn1, struct song_node * sn2)
 struct song_node * insert_sorted(struct song_node * sn){}
 struct song_node * find_song(char * artist, char * name){}
 struct song_node * find_first(char * artist){}
-struct song_node * get_rand(struct song_node * sn){
-  srand(time(NULL));
-  int randint = rand();
+
+struct song_node * get_randn(struct song_node * sn, int len)
+{
+	srand(time(NULL));
+	int randint = rand() * len;
+	int i = 0;
+	struct song_node * val = sn;
+
+	for(; i < randint; i++) {
+		val = sn->next;
+	}
+	return val;
 }
-struct song_node * remove_node(struct song_node * sn, char * artist, char * name) {
-  struct song_node * next = sn; //TODO: rename next
+
+struct song_node * remove_node(struct song_node * sn, char * artist, char * name)
+{
+  struct song_node * next = sn;
   // should_remove_val checks to see if the names and songs match
   int should_remove_val = (strncpy(next->name, name, 100) || name == NULL) &&
 	(strncpy(next->artist, artist, 100) || artist == NULL);
@@ -88,4 +103,15 @@ struct song_node * free_nullify(struct song_node *val) {
 	free(val);
 	val = NULL;
 	return NULL;
+}
+
+int len(struct song_node *sn) {
+	int len = 0;
+	struct song_node *node = sn;
+
+	while(node) {
+		len++;
+		node = node->next;
+	}
+	return len;
 }
